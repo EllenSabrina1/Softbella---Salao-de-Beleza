5405f12a47cbee00245888d358e475c95a8d511b
Ellen Sabrina <ellen.pereira@estudante.ifro.edu.br>
2024-09-25 00:16:57 -0400

    Telas

--- obj/Debug/ProjetoPDS-SoftBella_Content.g.i.cs
+++ obj/Debug/ProjetoPDS-SoftBella_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("recursos/imagens/mulher.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("recursos/imagens/escrever.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("recursos/imagens/lata-de-lixo.png")]
 
 
